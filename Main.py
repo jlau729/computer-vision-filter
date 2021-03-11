e7e87f11c90e1eafb4fc878e01f454fae17e55a7
Steven Luong <stluong@attu3.cs.washington.edu>
2021-03-10 22:11:53 -0800

    tried to train detector

--- Main.py
+++ Main.py
@@ -11,6 +11,11 @@ def create_model():
     for currentFile in glob.glob("./faces/train/face/*.pgm"):
         im = Image.open(currentFile)
         image_list.append((im, 1))
-    print(len(image_list))
+    for currentFile in glob.glob("./faces/train/non-face/*.pgm"):
+        im = Image.open(currentFile)
+        image_list.append((im, 0))
+    cascadeDetect = CascadeDetector()
+    cascadeDetect.train_model(image_list, 50, 50, 80)
+    print("done")
 
 create_model()
