89aee34e5ffb5f270162fb38692ab98d43aa7b38
Steven <stluong123@gmail.com>
2021-03-14 23:56:37 -0700

    Finished project

--- alternative/Main.py
+++ alternative/Main.py
@@ -4,10 +4,20 @@ import glob
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread("./selfie.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+# Censors the faces in the given image with the given sticker
+# original-image: string path to the image we want to censor the faces in
+# sticker_path: image we want to use to censor all the faces
+# returns a new image that is the result of the original image censored by the sticker
+def censor_face(original_image, sticker_path):
+    img = cv2.imread(original_image)
+    layered_copy = Image.open(original_image)
+    sticker = Image.open(sticker_path) # sticker we want to use to censor
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        sticker = sticker.resize((w, h))
+        layered_copy.paste(sticker, (x, y))
+    return layered_copy
 
-cv2.imwrite("result.jpg", img)
+censor_face("selfie.jpg", "../pokeball.png").save("censored_selfie.jpg", format = "png")
+censor_face("paris.jpg", "../pokeball.png").save("censored_paris.jpg", format = "png")
